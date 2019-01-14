e3e2a11184174b477a7383605f4625b89828b7eb
winterMute84 <wcarbajal84@gmail.com>
2019-01-13 16:05:49 -0800

    implemented scroll up arrow

--- index.js
+++ index.js
@@ -1,93 +1,117 @@
-var Messenger = function(el){
-    'use strict';
-    var m = this;
-    
-    m.init = function(){
-      m.codeletters = "&#*+%?£@§$";
-      m.message = 0;
-      m.current_length = 0;
-      m.fadeBuffer = false;
-      m.messages = [
-        'Welcome',
-        'I\'m glad you stopped by',
-        'Let\'s get started!' ,
-       
-      ];
-      
-      setTimeout(m.animateIn, 100);
-    };
-    
-    m.generateRandomString = function(length){
-      var random_text = '';
-      while(random_text.length < length){
-        random_text += m.codeletters.charAt(Math.floor(Math.random()*m.codeletters.length));
-      } 
-      
-      return random_text;
-    };
-    
-    m.animateIn = function(){
-      if(m.current_length < m.messages[m.message].length){
-        m.current_length = m.current_length + 2;
-        if(m.current_length > m.messages[m.message].length) {
-          m.current_length = m.messages[m.message].length;
-        }
-        
-        var message = m.generateRandomString(m.current_length);
-        $(el).html(message);
-        
-        setTimeout(m.animateIn, 20);
-      } else { 
-        setTimeout(m.animateFadeBuffer, 20);
+jQuery(document).ready(function ($) {
+
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 50) {
+      $('.scrolltop:hidden').stop(true, true).fadeIn();
+    } else {
+      $('.scrolltop').stop(true, true).fadeOut();
+    }
+  });
+  $(function () {
+    $(".scroll").click(function () {
+      $("html,body").animate({
+        scrollTop: $(".thetop").offset().top
+      }, "1000");
+      return false
+    })
+  })
+});
+
+
+
+var Messenger = function (el) {
+  'use strict';
+  var m = this;
+
+  m.init = function () {
+    m.codeletters = "&#*+%?£@§$";
+    m.message = 0;
+    m.current_length = 0;
+    m.fadeBuffer = false;
+    m.messages = [
+      'Welcome',
+      'I\'m glad you stopped by',
+      'Let\'s get started!',
+
+    ];
+
+    setTimeout(m.animateIn, 100);
+  };
+
+  m.generateRandomString = function (length) {
+    var random_text = '';
+    while (random_text.length < length) {
+      random_text += m.codeletters.charAt(Math.floor(Math.random() * m.codeletters.length));
+    }
+
+    return random_text;
+  };
+
+  m.animateIn = function () {
+    if (m.current_length < m.messages[m.message].length) {
+      m.current_length = m.current_length + 2;
+      if (m.current_length > m.messages[m.message].length) {
+        m.current_length = m.messages[m.message].length;
       }
-    };
-    
-    m.animateFadeBuffer = function(){
-      if(m.fadeBuffer === false){
-        m.fadeBuffer = [];
-        for(var i = 0; i < m.messages[m.message].length; i++){
-          m.fadeBuffer.push({c: (Math.floor(Math.random()*12))+1, l: m.messages[m.message].charAt(i)});
-        }
-      }
-      
-      var do_cycles = false;
-      var message = ''; 
-      
-      for(var i = 0; i < m.fadeBuffer.length; i++){
-        var fader = m.fadeBuffer[i];
-        if(fader.c > 0){
-          do_cycles = true;
-          fader.c--;
-          message += m.codeletters.charAt(Math.floor(Math.random()*m.codeletters.length));
-        } else {
-          message += fader.l;
-        }
-      }
-      
+
+      var message = m.generateRandomString(m.current_length);
       $(el).html(message);
-      
-      if(do_cycles === true){
-        setTimeout(m.animateFadeBuffer, 50);
+
+      setTimeout(m.animateIn, 20);
+    } else {
+      setTimeout(m.animateFadeBuffer, 20);
+    }
+  };
+
+  m.animateFadeBuffer = function () {
+    if (m.fadeBuffer === false) {
+      m.fadeBuffer = [];
+      for (var i = 0; i < m.messages[m.message].length; i++) {
+        m.fadeBuffer.push({
+          c: (Math.floor(Math.random() * 12)) + 1,
+          l: m.messages[m.message].charAt(i)
+        });
+      }
+    }
+
+    var do_cycles = false;
+    var message = '';
+
+    for (var i = 0; i < m.fadeBuffer.length; i++) {
+      var fader = m.fadeBuffer[i];
+      if (fader.c > 0) {
+        do_cycles = true;
+        fader.c--;
+        message += m.codeletters.charAt(Math.floor(Math.random() * m.codeletters.length));
       } else {
-        setTimeout(m.cycleText, 2000);
+        message += fader.l;
       }
-    };
-    
-    m.cycleText = function(){
-      m.message = m.message + 1;
-      if(m.message >= m.messages.length){
-        m.message = 0;
-      }
-      
-      m.current_length = 0;
-      m.fadeBuffer = false;
-      $(el).html('');
-      
-      setTimeout(m.animateIn, 200);
-    };
-    
-    m.init();
-  }
-  
-  console.clear();
-  var messenger = new Messenger($('#messenger'));
+    }
+
+    $(el).html(message);
+
+    if (do_cycles === true) {
+      setTimeout(m.animateFadeBuffer, 50);
+    } else {
+      setTimeout(m.cycleText, 2000);
+    }
+  };
+
+  m.cycleText = function () {
+    m.message = m.message + 1;
+    if (m.message >= m.messages.length) {
+      m.message = 0;
+    }
+
+    m.current_length = 0;
+    m.fadeBuffer = false;
+    $(el).html('');
+
+    setTimeout(m.animateIn, 200);
+  };
+
+  m.init();
+}
+
+console.clear();
+var messenger = new Messenger($('#messenger'));
